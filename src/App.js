10a01912ea05011e42217f5a83a60e092adad6c1
Tim DeCillis <timdecillis@gmail.com>
2023-09-19 16:40:17 -0700

    Rendered PostsList to page

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import {
 } from 'react-router-dom'
 
 import { Navbar } from './app/Navbar'
+import {PostsList} from './features/posts/PostsList'
 import Button from './components/Button';
 import Message from './components/Message';
 
@@ -26,9 +27,9 @@ function App() {
             exact
             path="/"
             render={() => (
-              <section>
-                <h2>Welcome to the Redux Essentials example app!</h2>
-              </section>
+              <React.Fragment>
+                <PostsList/>
+              </React.Fragment>
             )}
           />
           <Redirect to="/" />
