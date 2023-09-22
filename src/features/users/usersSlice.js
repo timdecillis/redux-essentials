dfc180e8a156297f46b949a4928b85aec58ba933
Tim DeCillis <timdecillis@gmail.com>
2023-09-22 11:43:17 -0700

    Added Author on AddPostForm

--- src/features/users/usersSlice.js
+++ src/features/users/usersSlice.js
@@ -5,3 +5,11 @@ const initialState = [
   { id: '1', name: 'Kevin Grant'},
   { id: '2', name: 'Madison Price'}
 ]
+
+const usersSlice = createSlice({
+  name: 'users',
+  initialState,
+  reducers: {}
+})
+
+export default usersSlice.reducer
