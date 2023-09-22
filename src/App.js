b8b1d363c37085c46aeffbcfc9179f4e6487b303
Tim DeCillis <timdecillis@gmail.com>
2023-09-21 19:04:19 -0700

    Created route to EditPostForm on App.js

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ import { Navbar } from './app/Navbar'
 import {PostsList} from './features/posts/PostsList'
 import {AddPostForm} from './features/posts/AddPostForm'
 import { SinglePostPage } from './features/posts/SinglePostPage'
+import { EditPostForm } from './features/posts/EditPostForm'
 import Button from './components/Button';
 import Message from './components/Message';
 
@@ -36,6 +37,7 @@ function App() {
             )}
           />
           <Route exact path="/posts/:postId" component={SinglePostPage} />
+          <Route exact path="/editPost/:postId" component={EditPostForm} />
           <Redirect to="/" />
         </Switch>
       </div>
