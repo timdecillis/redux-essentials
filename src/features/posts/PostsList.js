7a34da42908defc07e836d4b31a63160ce838cb3
Tim DeCillis <timdecillis@gmail.com>
2023-09-23 15:37:29 -0700

    Actually added date to posts

--- src/features/posts/PostsList.js
+++ src/features/posts/PostsList.js
@@ -3,6 +3,7 @@ import { useSelector } from 'react-redux'
 import { Link } from 'react-router-dom'
 
 import { PostAuthor } from './PostAuthor'
+import { TimeAgo } from './TimeAgo'
 
 export const PostsList = () => {
   const posts = useSelector(state => state.posts)
@@ -12,8 +13,11 @@ export const PostsList = () => {
   const renderedPosts = orderedPosts.map(post => (
     <article className="post-excerpt" key={post.id}>
       <h3>{post.title}</h3>
+      <div>
+        <PostAuthor userId={post.user} />
+        <TimeAgo timestamp={post.date} />
+      </div>
       <p className="post-content">{post.content.substring(0, 100)}</p>
-      <PostAuthor userId={post.user}/>
       <Link to={`/posts/${post.id}`} className="button muted-button">
         View Post
       </Link>
