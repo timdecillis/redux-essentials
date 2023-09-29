e62c63f4c16208badbda707ff2e79d8adaaf0ba2
Tim DeCillis <timdecillis@gmail.com>
2023-09-29 14:26:11 -0700

    Deleted comment

--- src/features/posts/postsSlice.js
+++ src/features/posts/postsSlice.js
@@ -53,5 +53,3 @@ export const selectAllPosts = state => state.posts.posts
 
 export const selectPostById = (state, postId) =>
   state.posts.posts.find(post => post.id === postId)
-
-// logging comment for fun
